242a015bb5e604411bf1955807f24d5a4aae617e
Nenad Stojanovikj <nekk1@live.com>
2016-04-18 01:12:24 +0200

    Shorten the list of possible commands. Add config:init command

--- src/TargetPHProcess/Command/Configuration/AuthConfigurationCommand.php
+++ src/TargetPHProcess/Command/Configuration/AuthConfigurationCommand.php
@@ -7,11 +7,26 @@ namespace TargetPHProcess\Command\Configuration;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use TargetPHProcess\BLL\Auth\AuthGenerator;
 
 class AuthConfigurationCommand extends Command
 {
+    /** @var AuthGenerator */
+    private $authGenerator;
+
+    public function __construct(AuthGenerator $authGenerator)
+    {
+        parent::__construct('config:auth');
+        $this->authGenerator = $authGenerator;
+    }
+
+    public function configure()
+    {
+        $this->setDescription('Changes the authentication for the current project');
+    }
+
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        
+        echo getcwd();
     }
 }
--- src/config/command_loader.php
+++ src/config/command_loader.php
@@ -7,6 +7,7 @@ function load_commands_to_app(Application $app, ContainerBuilder $container)
 {
     $ids = $container->findTaggedServiceIds('console.command');
     foreach ($ids as $service => $id) {
+        /** @var \Symfony\Component\Console\Command\Command $command */
         $command = $container->get($service);
         $app->add($command);
     }
