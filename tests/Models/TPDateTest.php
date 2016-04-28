626be4277b1306bd5245eccd1fa04dd1388f17cd
Nenad Stojanovikj <nekk1@live.com>
2016-04-28 23:56:15 +0200

    Add test for TPDate

---
+++ tests/Models/TPDateTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace tests\Models;
+
+
+use Exception;
+use TargetPHProcess\Models\TPDate;
+
+class TPDateTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function createDateFromMilliseconds()
+    {
+        date_default_timezone_set('GMT');
+        $tpdate = new TPDate('Date(1460379452000)');
+        $this->assertEquals('2016-04-11 12:57:32', $tpdate->format('Y-m-d H:i:s'));
+    }
+
+    /** @test */
+    public function createDateFromMillisecondsAndOffset()
+    {
+        date_default_timezone_set('GMT');
+        $tpdate = new TPDate('Date(1460379452000+0200)');
+        $this->assertEquals('2016-04-11 14:57:32', $tpdate->format('Y-m-d H:i:s'));
+    }
+
+    /**
+     * @test
+     * @expectedException Exception
+     */
+
+    public function failCreation()
+    {
+        $tpdate = new TPDate('Dat460379452000+0200');
+    }
+}
+
--- tests/SystemConfiguration/ConfigurationWriterTest.php
+++ tests/SystemConfiguration/ConfigurationWriterTest.php
@@ -13,19 +13,31 @@ class ConfigurationWriterTest extends \PHPUnit_Framework_TestCase
     /** @var vfsStreamDirectory */
     protected $root;
 
+    /** @var ConfigurationWriter */
+    private $instance;
+
     public function setUp()
     {
-        $this->root = vfsStream::setup('/', 755, ['home' => ['test']]);
-        $_SERVER['HOME'] = vfsStream::url('/home/test');
-        mkdir($_SERVER['HOME']);
+        $this->root = vfsStream::setup('test');
+        $_SERVER['HOME'] = vfsStream::url('test');
+        $this->instance = new ConfigurationWriter();
     }
 
     /** @test */
     public function checkIfConfigFileIsCreated()
     {
-        $instance = new ConfigurationWriter();
-        $this->assertTrue($this->root->hasChild('/home/test/.tpconfig'));
-        $json = file_get_contents(vfsStream::url('/home/test/.tpconfig'));
+        $this->assertTrue($this->root->hasChild('test/.tpconfig'));
+        $json = file_get_contents(vfsStream::url('test/.tpconfig'));
         $this->assertEquals('[]', $json);
+    }
+
+    /** @test */
+    public function checkIfWriteReadIsSuccess()
+    {
+        $json = '{"Id":"1"}';
+        $this->instance->writeConfig($json);
+        $readJson = $this->instance->readConfig();
+        $this->assertJson($json);
+        $this->assertEquals($json, $readJson);
     }
 }
